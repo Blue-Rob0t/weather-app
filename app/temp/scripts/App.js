661925e18532ac14af4fe6d9bbbb2b22523269ad
cd-zero <mike.aduayi@gmail.com>
2017-05-30 18:47:24 -0400

    text entry

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -75,7 +75,7 @@
 	    var lat = position.coords.latitude;
 	    var lon = position.coords.longitude;
 	    // api call based on lat/lon and global apikey variable to get data from api
-	    var theUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&APPID=' + DOM.APIKEY;
+	    var theUrl = location || 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&APPID=' + DOM.APIKEY;
 
 	    // API CALL- passes api call url + callback function
 	    function httpGetAsync(urlRequest, callbackFunc) {
@@ -172,8 +172,112 @@
 	  });
 	}
 
+	// TEXT ENTRY START
+	function geoTextEntry(location) {
+
+	  console.log('geoTextEntry!!!');
+	  var theUrl = location;
+
+	  // API CALL- passes api call url + callback function
+	  function httpGetAsync(urlRequest, callbackFunc) {
+	    // make a new httpRequest
+	    var xmlHttp = new XMLHttpRequest();
+	    // if request is successful the run parseData function
+	    xmlHttp.onreadystatechange = function () {
+	      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+	        console.log([xmlHttp.status, xmlHttp.readyState, "fetch status"]);
+	        parseData(xmlHttp.responseText);
+	      }
+	    };
+	    xmlHttp.open("GET", urlRequest, true); // true for asynchronous
+	    xmlHttp.send(null);
+	  }
+	  function parseData(apiData) {
+	    // apiData parsed from string to JSON data
+	    var apiData = JSON.parse(apiData);
+
+	    function visualize(apiData) {
+	      // bool value initialize for changeTemp function
+	      var bool = true;
+
+	      console.log(apiData);
+	      DOM.LOCATION.innerHTML = '' + apiData.name;
+	      DOM.TEMP.innerHTML = Math.round(apiData.main.temp * 9 / 5 - 459.67) + ' F\xB0';
+	      // DOM.BUTTON2.innerHTML  ="click for C"
+	      DOM.DESC.innerHTML = '' + apiData.weather[0].description;
+	      // DOM.IMG.src = `http://openweathermap.org/img/w/${apiData.weather[0].icon}.png`
+	      // Background Image
+	      var descriptions = {
+	        "clear sky": "sunny-day",
+	        "few clouds": "scattered-clouds",
+	        "overcast clouds": "over-cast",
+	        "scattered clouds": "scattered-clouds",
+	        "broken clouds": "cloudy",
+	        "rain": "light-rain",
+	        "light rain": "light-rain",
+	        "thunderstorm": "heavy-rain",
+	        "snow": "snow",
+	        "mist": "mist",
+	        "moderate rain": 'light-rain',
+	        "heavy intensity rain": 'light-rain',
+	        'light intensity drizzle': 'light-rain'
+	      };
+
+	      for (var key in descriptions) {
+	        if (key == '' + apiData.weather[0].description.toLowerCase()) {
+	          //   DOM.HTML.style.backgroundImage = `url('/assets/images/${descriptions[key]}.jpg')`
+	          //   DOM.HTML.style.backgroundRepeat = "no-repeat";
+	          // console.log(DOM.HTML);
+	          var source = document.createElement("source");
+	          source.setAttribute('src', '/assets/video/' + descriptions[key] + '.mp4');
+	          DOM.VID.appendChild(source);
+	          DOM.VID.loop = true;
+	          DOM.VID.play();
+
+	          var iconHTML = document.querySelector('.' + descriptions[key]);
+
+	          iconHTML.classList.remove("visible");
+	          iconHTML.classList.add('visible');
+
+	          // console.log(iconHTML);
+	        }
+	      }
+
+	      if (mq.matches) {
+	        // window width is at least 500px
+	      } else {}
+	        // window width is less than 500px
+
+	        // Toggle between F and C
+	      function changeTemp() {
+	        // resets value of bool
+	        bool = !bool;
+
+	        if (bool) {
+	          // DOM.BUTTON2.innerHTML  ="click for C"
+	          DOM.TEMP.innerHTML = Math.round(apiData.main.temp * 9 / 5 - 459.67) + ' F\xB0';
+	        } else {
+	          // DOM.BUTTON2.innerHTML  ="click for F"
+	          DOM.TEMP.innerHTML = Math.round(apiData.main.temp - 273.15) + ' C\xB0';
+	        }
+	      }
+
+	      DOM.TEMP.addEventListener('click', changeTemp);
+	    }
+	    // passes in apiData and visualizes it to page
+	    visualize(apiData);
+	  }
+	  // API CALL- passes api call url + callback function
+	  httpGetAsync(theUrl, parseData);
+	}
+	// TEXT ENTRY END
+
+
 	function navControl() {
 
+	  var menu = document.querySelector('.to_nav');
+	  var navigation = document.querySelector('.navigator');
+	  var navLinks = document.querySelector('ul> li');
 	  var itemsChange = document.getElementsByClassName('hidden');
 
 	  function showContent() {
@@ -189,10 +293,6 @@
 	    }
 	  }
 
-	  var menu = document.querySelector('.to_nav');
-	  var navigation = document.querySelector('.navigator');
-	  var navLinks = document.querySelector('ul> li');
-
 	  menu.addEventListener('click', function () {
 	    nav = !nav;
 	    if (nav) {
@@ -206,12 +306,14 @@
 	    }
 	  });
 	}
-
-	document.querySelector('.icon-bg').addEventListener("click", geoLocation);
 	// function that findes m0achine position if user gives access and
 	// initialize weather application
+
 	geoLocation();
+
 	navControl();
+
+	document.querySelector('.icon-bg').addEventListener("click", geoLocation);
 
 	// Gets data and spreads it into array cities
 
@@ -254,22 +356,28 @@
 	  // sets up a ul most likely that containts the list items
 	  suggestions.innerHTML = html.toString();
 	}
+
 	function cityName() {
 
 	  // submit button
 	  document.querySelector('#submit').addEventListener('click', function () {
 	    var inputValue = searchInput.value;
-	    var apicall = 'api.openweathermap.org/data/2.5/weather?q=' + inputValue + ',us';
-	    geoLocation(apicall);
+	    var apicall = 'api.openweathermap.org/data/2.5/weather?q=' + inputValue + ',us&APPID=' + DOM.APIKEY;
+
+	    console.log(apicall);
+	    geoTextEntry(apicall);
+
+	    // geoLocation(apicall);
 	  });
 
 	  suggestions.addEventListener('click', function (e) {
-
 	    // console.log(e.target.id);
 	    if (e.target.id = 'list') {
 	      var inputValue = e.target.innerText.split(',')[0];
-	      var apicall = 'api.openweathermap.org/data/2.5/weather?q=' + inputValue + ',us';
-	      geoLocation(apicall);
+	      var apicall = 'api.openweathermap.org/data/2.5/weather?q=' + inputValue + ',us&APPID=' + DOM.APIKEY;
+	      console.log(apicall);
+	      geoTextEntry(apicall);
+	      // geoLocation(apicall)
 	    } else {
 	      console.log("error");
 	    }
